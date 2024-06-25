1432f2f71338ef7c0f537120e2011685888c8e51
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-25 17:20:41 -0300

    feat: add main method in BankApplication

--- src/main/java/com/wesleybertipaglia/bank/BankApplication.java
+++ src/main/java/com/wesleybertipaglia/bank/BankApplication.java
@@ -2,5 +2,10 @@ package com.wesleybertipaglia.bank;
 
 public class BankApplication {
 	public static void main(String[] args) {
+		Client me = new Client("Jhon", "jhon@mail.com", "+00 (00)00000-0000", "5 Avenue Street, 1029 - New York");
+		Account myAccount = new Account(me);
+		myAccount.deposit(10000);
+		myAccount.withdraw(500);
+		myAccount.statement();
 	}
 }
